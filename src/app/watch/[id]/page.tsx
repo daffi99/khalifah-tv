b1835fb980073b7eab93851079f04a57333da98d
daffiya <daffiyashir@gmail.com>
2026-06-02 12:31:14 +0700

    style: change video player wrapper background to white for seamless light mode

--- src/app/watch/[id]/page.tsx
+++ src/app/watch/[id]/page.tsx
@@ -55,10 +55,10 @@ export default async function WatchPage({ params }: WatchPageProps) {
       <div className="w-full bg-white relative flex flex-col h-[100dvh] overflow-hidden">
         
         {/* Top Section: Player & Info */}
-        <div className="w-full flex flex-col shrink-0">
+        <div className="w-full flex flex-col shrink-0 bg-white">
           {/* Fixed Player at the Top */}
-          <div className="w-full bg-black aspect-video landscape:max-h-[55dvh] landscape:aspect-auto flex justify-center shrink-0 shadow-sm relative z-20">
-            <div className="w-full h-full max-w-5xl flex justify-center bg-black">
+          <div className="w-full bg-white aspect-video landscape:max-h-[55dvh] landscape:aspect-auto flex justify-center shrink-0 shadow-sm relative z-20">
+            <div className="w-full h-full max-w-5xl flex justify-center bg-white">
               <KidsWatchPlayer 
                 title={video.title} 
                 src={video.video_url} 
